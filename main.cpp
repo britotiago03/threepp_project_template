3ea462ca8a79164d39abdf58cb5fa8f71b77cfeb
britotiago03 <69848652+britotiago03@users.noreply.github.com>
2023-05-03 09:55:29 +0200

    Init commit

---
+++ main.cpp
@@ -0,0 +1,67 @@
+
+#include "threepp/extras/imgui/imgui_context.hpp"
+#include "threepp/threepp.hpp"
+
+using namespace threepp;
+
+namespace {
+
+    auto createBox(const Vector3& pos, const Color& color) {
+        auto geometry = BoxGeometry::create();
+        auto material = MeshBasicMaterial::create();
+        material->color.copy(color);
+        auto mesh = Mesh::create(geometry, material);
+        mesh->position.copy(pos);
+
+        return mesh;
+    }
+
+}// namespace
+
+int main() {
+
+    Canvas canvas("threepp demo", {{"antialiasing", 4}});
+    GLRenderer renderer(canvas);
+    renderer.setClearColor(Color::aliceblue);
+
+    auto camera = PerspectiveCamera::create();
+    camera->position.z = 5;
+
+    OrbitControls controls{camera, canvas};
+
+    auto scene = Scene::create();
+
+    auto group = Group::create();
+    group->add(createBox({-1, 0, 0}, Color::green));
+    group->add(createBox({1, 0, 0}, Color::blue));
+    scene->add(group);
+
+    renderer.enableTextRendering();
+    auto& textHandle = renderer.textHandle("Hello World");
+    textHandle.setPosition(0, canvas.getSize().height - 30);
+    textHandle.scale = 2;
+
+    std::array<float, 3> posBuf{};
+    imgui_functional_context ui(canvas.window_ptr(), [&] {
+        ImGui::SetNextWindowPos({0, 0}, 0, {0, 0});
+        ImGui::SetNextWindowSize({230, 0}, 0);
+        ImGui::Begin("Demo");
+        ImGui::SliderFloat3("position", posBuf.data(), -1.f, 1.f);
+        controls.enabled = !ImGui::IsWindowHovered();
+        ImGui::End();
+    });
+
+    canvas.onWindowResize([&](WindowSize size) {
+        camera->aspect = size.getAspect();
+        camera->updateProjectionMatrix();
+        renderer.setSize(size);
+        textHandle.setPosition(0, size.height - 30);
+    });
+
+    canvas.animate([&] {
+        renderer.render(scene, camera);
+
+        ui.render();
+        group->position.fromArray(posBuf);
+    });
+}
